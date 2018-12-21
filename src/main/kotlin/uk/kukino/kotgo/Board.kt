17f534f9eb156e4a5ea3cd34f934b7be1a2951bd
Ed Riccardi <ed@kukino.uk>
2018-12-21 01:20:40 +0000

    cleanup

--- src/main/kotlin/uk/kukino/kotgo/Board.kt
+++ src/main/kotlin/uk/kukino/kotgo/Board.kt
@@ -4,28 +4,25 @@ enum class Color {
     BLACK, WHITE, EMPTY
 }
 
-data class Cell(val x: Int, val y: Int) {
+data class Cross(val x: Int, val y: Int) {
     fun idx(size: Int) = y * size + x
 }
 
-class Board {
+class Board(var size: Int, var handicap: Int = 0) {
 
-    var size: Int
-    var handicap: Int
-    var cells: Array<Color>
+    var intersections: Array<Color>
 
-    constructor(size: Int, handicap: Int = 0) {
-        if (size < 4) throw IllegalArgumentException("Board size should be at least 4 cells wide")
-        this.size = size
-        this.cells = Array(size * size) { Color.EMPTY }
-        this.handicap = handicap
+    init {
+        if (size < 4) throw IllegalArgumentException("Board size should be at least 4 intersections wide")
+        if (size >= 50) throw IllegalArgumentException("Board size too big")
+        this.intersections = Array(size * size) { Color.EMPTY }
+        //TODO: handicaps here
     }
 
+    fun get(cross: Cross) = intersections[cross.idx(size)]
 
-    fun get(cell: Cell) = cells[cell.idx(size)]
-
-    fun set(cell: Cell, col: Color) {
-        cells[cell.idx(size)] = col
+    fun set(cross: Cross, col: Color) {
+        intersections[cross.idx(size)] = col
     }
 
 }
--- src/test/kotlin/uk/kukino/kotgo/BoardTest.kt
+++ src/test/kotlin/uk/kukino/kotgo/BoardTest.kt
@@ -1,8 +1,8 @@
 package uk.kukino.kotgo
 
 import kotlin.test.Test
-import kotlin.test.fail
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class BoardTest {
 
@@ -12,15 +12,19 @@ class BoardTest {
             try {
                 Board(size)
                 fail("A board size $size should not be possible to create")
-            } catch (e :IllegalArgumentException) {
-                // good
+            } catch (e: IllegalArgumentException) {
             }
         }
     }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun boardInit_sizeShouldBeLessThan50() {
+        Board(50)
+    }
+
     @Test
     fun boardInit_validSizeAllOk() {
-        for (size in 4 until 25) {
+        for (size in 4 until 50) {
             val board = Board(size)
             assertEquals(size, board.size)
         }
@@ -32,7 +36,7 @@ class BoardTest {
             val board = Board(size)
             for (x in 0 until board.size) {
                 for (y in 0 until board.size) {
-                    assertEquals(Color.EMPTY, board.get(Cell(x, y)))
+                    assertEquals(Color.EMPTY, board.get(Cross(x, y)))
                 }
             }
         }
@@ -41,7 +45,7 @@ class BoardTest {
     @Test
     fun boardBasic_setOne() {
         val board = Board(19)
-        val cell = Cell(5,5)
+        val cell = Cross(5, 5)
         board.set(cell, Color.BLACK)
         assertEquals(Color.BLACK, board.get(cell))
     }
